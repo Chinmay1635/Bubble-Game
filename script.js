c89d165da93410b6667fe8f7bb72a3caeb77fe21
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-05-30 19:27:28 +0530

    completed...Loader Remaining

---
+++ script.js
@@ -0,0 +1,69 @@
+var timer = 30;
+var score = 0;
+var gameArea = document.querySelector("#pbtm");
+var timerDisplay = document.querySelector("#timer");
+var scoreDisplay = document.querySelector("#score")
+var hitDisplay = document.querySelector("#hit");
+var hitrn;
+
+function bubbleMaker(){
+    var clutter = '';
+
+for(var i=1; i<=132; i++){
+    var rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`
+}
+
+    gameArea.innerHTML=clutter;
+}
+
+function timerFunction(){
+  var timeInt =  setInterval(function(){
+        if(timer>=0){
+            timerDisplay.textContent = timer;
+            timer--;
+        }else{
+            clearInterval(timeInt);
+            gameArea.innerHTML = `<h1>Game Over</h1>`
+        }
+    },1000)
+}
+
+function scoreUpdater(){
+    score +=10;
+    scoreDisplay.textContent = score;
+}
+
+function hitMaker(){
+    hitrn = Math.floor(Math.random()*10);
+    hitDisplay.textContent = hitrn;
+}
+
+bubbleMaker();
+timerFunction();
+hitMaker();
+function mainFunction(){
+    gameArea.addEventListener("click",function(data){
+        var userNumber = Number(data.target.textContent);
+        if(userNumber===hitrn){
+            data.target.style.backgroundColor="rgb(0, 255, 0)";
+            setTimeout(function(){
+                scoreUpdater();
+                bubbleMaker();
+                hitMaker();
+            },500)
+        }
+        else if(userNumber!=hitrn && data.target.id!="pbtm"){
+            data.target.style.backgroundColor="rgb(255, 0, 0)";
+            setTimeout(function(){
+                bubbleMaker();
+                hitMaker();
+            },500)
+        }
+
+    })
+}
+mainFunction()
+
+
+
